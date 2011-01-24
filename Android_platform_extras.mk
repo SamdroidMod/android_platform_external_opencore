9ac6e143a228baaf9fdebe746218697e16085c58
LeshaK <leshak@leshak.ru>
2011-01-24 15:24:44 +0300

    Revert "add BOARD_USES_ECLAIR_LIBCAMERA"
    This reverts commit 29706b39cccce3212df4bfd0ec8869d5029741cb.

--- Android_platform_extras.mk
+++ Android_platform_extras.mk
@@ -10,10 +10,6 @@ LOCAL_SHARED_LIBRARIES :=	libutils \
 							libvorbisidec \
 							libsonivox \
 							liblog \
-							libicuuc
-
-ifneq ($(BOARD_USES_ECLAIR_LIBCAMERA),true)
-    LOCAL_SHARED_LIBRARIES += \
-	libsurfaceflinger_client \ 
-	libcamera_client
-endif
+							libicuuc \
+							libcamera_client \
+							libsurfaceflinger_client
